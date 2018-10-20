0d758dbb2542656f096d183c4ac6be7cbaad1a5c
Phoenix <kahlo.phoenix@gmail.com>
2018-10-20 16:02:48 -0400

    begin laying out prototype 3

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,108 @@ pub struct ChildId {
 #[derive(Debug, Copy, Clone)]
 pub struct InvalidChildIndex(pub usize);
 
+enum Node<T, C: FixedSizeArray<ChildId>> {
+    Garbage,
+    Present {
+        elem: UnsafeCell<T>,
+        parent: Cell<ParentId>,
+        children: UnsafeCell<C>
+    }
+}
+
+#[derive(Copy, Clone, Eq, PartialEq, Debug)]
+enum ParentId {
+    Some(usize),
+    Root,
+    Detached,
+}
+
+pub struct Tree<T, C: FixedSizeArray<ChildId>> {
+    nodes: Vec<UnsafeCell<Node<T, C>>>,
+    root: Option<usize>,
+    garbage: Vec<usize>,
+}
+impl<T, C: FixedSizeArray<ChildId>> Tree<T, C> {
+    pub fn new() -> Self {
+        Tree {
+            nodes: Vec::new(),
+            root: None,
+            garbage: Vec::new()
+        }
+    }
+}
+
+pub struct NodeWriteGuard<'tree, 'node: 'tree, T, C: FixedSizeArray<ChildId>> {
+    tree: &'tree mut Tree<T, C>,
+    index: usize,
+
+    p1: PhantomData<&'node mut ()>,
+}
+impl<'tree, 'node: 'tree, T, C: FixedSizeArray<ChildId>> NodeWriteGuard<'tree, 'node, T, C> {
+    pub fn split<'child>(&'child mut self) -> (&'child mut T, ChildGuard<'tree, 'child, T, C>) {
+        unsafe {
+            if let &Node::Present {
+                ref elem,
+                ref children,
+                ..
+            } = &*self.tree.nodes[self.index].get() {
+                let elem: &'child mut T = &mut*elem.get();
+                let children: &'child mut C = &mut*children.get();
+                let child_guard: ChildGuard<'tree, 'child, T, C> = ChildGuard {
+                    tree: self.tree,
+                    children
+                };
+                (elem, child_guard)
+            } else {
+                unreachable!("guarding garbage")
+            }
+        }
+    }
+}
+
+pub struct NodeOwnedGuard<'tree, T, C: FixedSizeArray<ChildId>> {
+    tree: &'tree mut Tree<T, C>,
+    index: usize,
+}
+impl<'tree, T, C: FixedSizeArray<ChildId>> NodeOwnedGuard<'tree, T, C> {
+    pub fn split<'child>(&'child mut self) -> (&'child mut T, ChildGuard<'tree, 'child, T, C>) {
+        unsafe {
+            if let &Node::Present {
+                ref elem,
+                ref children,
+                ..
+            } = &*self.tree.nodes[self.index].get() {
+                let elem: &'child mut T = &mut*elem.get();
+                let children: &'child mut C = &mut*children.get();
+                let child_guard: ChildGuard<'tree, 'child, T, C> = ChildGuard {
+                    tree: self.tree,
+                    children
+                };
+                (elem, child_guard)
+            } else {
+                unreachable!("guarding garbage")
+            }
+        }
+    }
+}
+
+//pub struct NodeOwnedGuard<>
+
+pub struct ChildGuard<'tree, 'node: 'tree, T, C: FixedSizeArray<ChildId>> {
+    tree: &'tree mut Tree<T, C>,
+    children: &'node mut C,
+}
+impl<'tree, 'node: 'tree, T, C: FixedSizeArray<ChildId>> ChildGuard<'tree, 'node, T, C> {
+    pub fn write_child<'child>(&'child mut self) -> Option<NodeWriteGuard<'tree, 'child, T, C>> {
+        unimplemented!()
+    }
+
+    pub fn take_child(&mut self) -> Option<NodeOwnedGuard<'tree, T, C>> {
+        unimplemented!()
+    }
+}
+
+/*
 struct Node<T, C: FixedSizeArray<ChildId>> {
     user_data: T,
     parent: Option<usize>,
@@ -148,6 +250,7 @@ impl<'tree, 'node, T, C: FixedSizeArray<ChildId>> NodeWriteGuard<'tree, 'node, T
         unimplemented!()
     }
 }
+*/
 */
 
 fn main() {
