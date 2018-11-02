8240f2afe6b82695dc41d1a5726a4ea8401e7820
Phoenix <kahlo.phoenix@gmail.com>
2018-11-02 00:49:47 -0500

    progress

--- src/test.rs
+++ src/test.rs
@@ -1,22 +1,2 @@
 
 use super::*;
-
-#[test]
-fn bad_test() {
-    let mut tree = Tree::<i32, [ChildId; 2]>::new();
-    let mut op = tree.operation();
-    op.put_root_elem(0);
-
-    let mut a = op.write_root().unwrap();
-    let ae = a.elem();
-    let mut b = op.write_root().unwrap();
-    let be = b.elem();
-
-    println!("{}", ae);
-    println!("{}", be);
-
-    *ae += 1;
-
-    println!("{}", ae);
-    println!("{}", be);
-}
